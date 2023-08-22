c17db0438794ddb5a54a9b5b5fc91bbf92379a87
christyuda <yudacihuy09@gmail.com>
2023-08-22 21:52:36 +0700

    update environment variable

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"os"
 	"taskbego/db"
 	"taskbego/handlers"
 
@@ -11,7 +12,14 @@ import (
 func main() {
 	app := fiber.New()
 
-	err := db.ConnectMongoDB("mongodb+srv://chrisyuda:yudacihuy123@clusterdb.xld4k4m.mongodb.net/", "dbgolang", "tasks")
+	// Mengambil nilai environment variable MONGODB_URI
+	mongoURI := os.Getenv("mongodb+srv://chrisyuda:yudacihuy123@clusterdb.xld4k4m.mongodb.net/")
+
+	if mongoURI == "" {
+		log.Fatal("MONGODB_URI environment variable is not set")
+	}
+
+	err := db.ConnectMongoDB(mongoURI, "dbgolang", "tasks")
 	if err != nil {
 		log.Fatal("Error connecting to MongoDB:", err)
 	}
@@ -23,5 +31,10 @@ func main() {
 	app.Delete("/tasks/:id", handlers.DeleteTask)
 	app.Get("/tasks/:id/detail", handlers.GetDetailTaskByID)
 
-	log.Fatal(app.Listen(":3000"))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000" // Default port
+	}
+
+	log.Fatal(app.Listen(":" + port))
 }
